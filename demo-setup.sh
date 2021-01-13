2f5982911fa6eedfe0cc3b49eea926097b2d9e99
Jean-Robin Foehn <jrfoehn@gmail.com>
2021-01-13 14:56:23 +0100

    modified script

--- demo-setup.sh
+++ demo-setup.sh
@@ -1,29 +1,29 @@
 ##!/bin/bash
-CLUSTER_NAME=YOUR_EKS_CLUSTER_NAME
+CLUSTER_NAME=demo-cluster
 
 ################
 # EKS Controls #
 ################
 
 # install OpenSSL
-yum install openssl -y
+# yum install openssl -y
 
-# install eksctl
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-sudo mv -v /tmp/eksctl /usr/local/bin
-eksctl completion bash >> ~/.bash_completion
-. /etc/profile.d/bash_completion.sh
-. ~/.bash_completion
+# # install eksctl
+# curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+# sudo mv -v /tmp/eksctl /usr/local/bin
+# #eksctl completion bash >> ~/.bash_completion
+# #. /etc/profile.d/bash_completion.sh
+# #. ~/.bash_completion
 
-# install kubectl
-curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+# # install kubectl
+# curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl
+# chmod +x ./kubectl
+# mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+# echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 
-# install Helm
-curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-helm version --short
+# # install Helm
+# curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+# helm version --short
 
 ######################
 # Launch EKS Cluster # 
